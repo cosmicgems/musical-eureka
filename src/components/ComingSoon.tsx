c275eefd255d5353fee83b67687805cc22dd861e
Maliek Davis <maliekjdavis24@gmail.com>
2023-10-09 16:22:59 -0400

    under construction responsive

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -7,14 +7,17 @@ const ComingSoon = () => {
   return (
     <Box sx={{bgcolor: grey[700]}}>
         <Layout>
-            <div className='flex flex-col gap-3 mt-12 pt-12'>
+            <div className='flex flex-col gap-3 mt-12 pt-12 justify-center items-center'>
                 <Typography sx={{}} variant='h2' className='gradient-text-five w-[100%] text-center font-bold'>
                     Coming Soon
                 </Typography>    
                 <CardMedia
+                className='sm:w-[35%] sm:rounded'
+                sx={{objectFit: "cover"}}
                     component="img"
                     image='https://www.transplantchild.eu/wp-content/uploads/image001.gif'
-                />             
+                />              
+          
             </div>
 
         </Layout>
