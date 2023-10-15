c0b179489ee63c4b6138ba45ddf11b5df70fd45d
Maliek Davis <maliekjdavis24@gmail.com>
2023-10-15 01:47:22 -0400

    app coming soon

--- src/pages/apps/ai-skills-team/index.tsx
+++ src/pages/apps/ai-skills-team/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppComingSoon from '../../../components/AppComingSoon'
 
 const AiSkillsTeam = () => {
-    const data = "AI Skills Team"
+    const data = {name: "AI Skills Team"}
   return (
         <AppComingSoon data={data} />
   )
