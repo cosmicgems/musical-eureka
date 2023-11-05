468f0cfb5ae21b94f164073e97982a913d61a311
Maliek Davis <maliekjdavis24@gmail.com>
2023-11-04 21:51:47 -0400

    load more working after breaking it down and putting it in it's own separate component

---
+++ src/components/Store/Products Page/ProductContainer.tsx
@@ -0,0 +1,182 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import ProductCard from '../Products/ProductCard';
+import { Box, Button, Typography } from '@mui/material'
+
+
+const ProductContainer = ({products}) => {  
+    const router = useRouter()
+    
+    const goToProductPage = productHandle => router.push(`/store/products/product/${productHandle}`);
+    const [loadedProducts, setLoadedProducts] = useState<any>([]);
+    const [hasNextPage, setHasNextPage] = useState<boolean>(null);
+    const [loading, setLoading] =useState<boolean>(false);
+    const [cursor, setCursor] = useState<string>(products[products.length - 1].cursor)
+  
+    const loadMoreRef = useRef<HTMLDivElement>();
+    const loadMoreTwoRef = useRef<HTMLDivElement>(null);
+
+
+    const loadMoreProducts = useCallback(async ( hasNextPage, ) => {
+    const data = {first: 24, after: cursor}
+    try {
+        
+        if (hasNextPage || hasNextPage === null) {
+            setLoading(true);
+            const res =  await axios.post("/api/store/load-more-products", {data});
+            console.log(res.data);
+
+            
+
+        
+            setLoadedProducts((prevLoadedProducts) => [
+                ...prevLoadedProducts,
+                ...res.data.products.data.products.edges.map((pro) => pro)
+            ]);
+
+            if(res.data.products.data.products.pageInfo.hasNextPage){
+                setHasNextPage(true);
+                setCursor(res.data.products.data.products.pageInfo.endCursor)
+                setLoading(false)
+            } else {
+                setHasNextPage(false);
+                setCursor(res.data.products.data.products.pageInfo.endCursor)
+                setLoading(false)
+            }
+    } else {
+        // All blogs are loaded
+        console.log('All blogs are loaded.');
+    }
+    } catch (error) {
+        console.error('Error fetching more blogs:', error);
+    } finally {
+        setLoading(false);
+    }
+    }, [cursor]);
+
+    useEffect(() => {
+        console.log(loadMoreRef);
+
+        if (!loadMoreRef?.current) return;
+
+        // if(!hasNextPage) return;
+        const observer = new IntersectionObserver(
+        (entries) => {
+            console.log(entries);
+            if (entries[0].isIntersecting) {
+            // Load more blogs when the target div becomes visible
+            loadMoreProducts(hasNextPage);
+            }
+        },
+        { threshold: 0.2 } // Adjust the threshold as needed
+        );
+
+        if (loadMoreRef.current) {
+        observer.observe(loadMoreRef.current);
+        console.log("It triggered");
+    }
+
+    const currentRef = loadMoreRef.current;
+
+    return () => {
+    if (currentRef) {
+        observer.unobserve(currentRef);
+    }
+    };
+    }, [loadMoreRef, loadMoreProducts, hasNextPage]);
+
+    useEffect(() => {
+    if(!loadMoreTwoRef?.current) return;
+
+    if(hasNextPage === false) return;
+    const observer = new IntersectionObserver(
+        (entries) => {
+            if (entries[0].isIntersecting) {
+                
+                // Load more blogs when the target div becomes visible
+                loadMoreProducts(hasNextPage);
+            }
+        },
+        { threshold: 0.1 } // Adjust the threshold as needed
+    );
+
+    if (loadMoreTwoRef.current) {
+        observer.observe(loadMoreTwoRef.current);
+                console.log("It triggered");
+    }
+    let w = loadMoreTwoRef.current
+    return () => {
+        if (w) {
+            observer.unobserve(w);
+        }
+    };
+    }, [ hasNextPage, loadMoreProducts]);
+
+    
+  const productsRef = useRef<HTMLDivElement>(null);
+
+    return (
+            <>
+            {
+                products.map((product, i) => {
+                if(loadedProducts.length === 0 && i === products.length - 1) {
+                    return (
+                    <div className='flex' key={` ${product.node.id} productsPage`}>
+                        <ProductCard goToProductPage={goToProductPage} product={product} />
+                    
+                        <div ref={loadMoreRef} className='h-[20vh] w-[20vw]'>
+                            <Typography variant='caption' className='gradient-text-home' sx={{}} component="div">
+                            Loading more products...
+                            </Typography>
+                        </div>                                
+                
+                    </div>
+                    )
+                }
+                
+                return (
+                    <div key={` ${product.node.id} productsPage`}>
+                    <ProductCard goToProductPage={goToProductPage} product={product} />
+                    </div>
+                )
+                })
+            }
+            {
+                loadedProducts.length > 0 &&
+                <>
+                {
+                    loadedProducts.map((product, i) => {
+                    if(loadedProducts.length > 0 && i === loadedProducts.length - 1) {
+                    return (
+                        <div className='flex' key={` ${product.node.id} productsPage`}>
+                        <ProductCard goToProductPage={goToProductPage} product={product} />
+                        {
+                            !hasNextPage ?
+                            null :
+                        <div ref={loadMoreRef} className='h-full flex justify-center items-center'>
+                            <Typography variant='caption' className='gradient-text-home' sx={{}} component="div">
+                            Loading more products...
+                            </Typography>
+                        </div>                            
+                        }
+
+                        </div>
+                    )
+                    }
+                    return (
+                        <div key={` ${product.node.id} productsPage`}>
+                        <ProductCard goToProductPage={goToProductPage} product={product} />
+                        </div>
+                    )
+                    })
+                }                      
+                </>
+            }            
+            </>
+          
+
+    )
+}
+
+export default ProductContainer
--- src/pages/store/products/index.tsx
+++ src/pages/store/products/index.tsx
@@ -9,138 +9,17 @@ import { AllProducts, NextProducts, callShopify } from '../../../../helpers/shop
 import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
 import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
 import axios from 'axios'
+import ProductContainer from '../../../components/Store/Products Page/ProductContainer'
 
 const ProductsPage = ({products}) => {
-  const [loadedProducts, setLoadedProducts] = useState<any>([]);
-  const [hasNextPage, setHasNextPage] = useState<boolean>(null);
-  const [loading, setLoading] =useState<boolean>(false);
-  const [cursor, setCursor] = useState<string>(products[products.length - 1].cursor)
-
-  const loadMoreRef = useRef<HTMLDivElement>(null);
-  const loadMoreTwoRef = useRef<HTMLDivElement>(null);
-
-const lastProductIndex = loadedProducts.length + products.length - 1
 
   const router = useRouter()
 
   const goToProductPage = productHandle => router.push(`/store/products/product/${productHandle}`);
 
 
-  const loadMoreProducts = useCallback(async ( hasNextPage, ) => {
-    
-    const data = {first: 24, after: cursor}
-    try {
-        
-        if (hasNextPage || hasNextPage === null) {
-            setLoading(true);
-            const res =  await axios.post("/api/store/load-more-products", {data});
-            console.log(res.data);
 
-            
 
-        
-            setLoadedProducts((prevLoadedProducts) => [
-              ...prevLoadedProducts,
-              ...res.data.products.data.products.edges.map((pro) => pro)
-            ]);
-
-            if(res.data.products.data.products.pageInfo.hasNextPage){
-              setHasNextPage(true);
-              setCursor(res.data.products.data.products.pageInfo.endCursor)
-              setLoading(false)
-            } else {
-              setHasNextPage(false);
-              setCursor(res.data.products.data.products.pageInfo.endCursor)
-              setLoading(false)
-            }
-    } else {
-        // All blogs are loaded
-        console.log('All blogs are loaded.');
-    }
-    } catch (error) {
-        console.error('Error fetching more blogs:', error);
-    } finally {
-        setLoading(false);
-    }
-}, [cursor]);
-
-useEffect(() => {
-  if(!loadMoreRef?.current) return;
-  
-  // if(!hasNextPage) return;
-  const observer = new IntersectionObserver(
-      (entries) => {
-          if (entries[0].isIntersecting) {
-              
-              // Load more blogs when the target div becomes visible
-              loadMoreProducts( hasNextPage);
-          }
-      },
-      { threshold: 0.2 } // Adjust the threshold as needed
-  );
-
-  if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
-              console.log("It triggered");
-  }
-
-  const v = loadMoreRef.current
-
-  return () => {
-      if (v) {
-          observer.unobserve(v);
-      }
-  };
-}, [ hasNextPage, loadMoreProducts]);
-
-useEffect(() => {
-  if(!loadMoreTwoRef?.current) return;
-  
-  if(hasNextPage === false) return;
-  const observer = new IntersectionObserver(
-      (entries) => {
-          if (entries[0].isIntersecting) {
-              
-              // Load more blogs when the target div becomes visible
-              loadMoreProducts(hasNextPage);
-          }
-      },
-      { threshold: 0.1 } // Adjust the threshold as needed
-  );
-
-  if (loadMoreTwoRef.current) {
-      observer.observe(loadMoreTwoRef.current);
-              console.log("It triggered");
-  }
-  let w = loadMoreTwoRef.current
-  return () => {
-      if (w) {
-          observer.unobserve(w);
-      }
-  };
-}, [ hasNextPage, loadMoreProducts]);
-
-useEffect(()=> {
-  console.log(loadedProducts);
-  
-}, [loadedProducts])
-
-  const prod = async (e, cursor) => {
-    console.log("clicked", cursor);
-    const data = {first: 24, after: cursor}
-    // e.preventDefault()
-    console.log(data);
-    
-    const p = await axios.post("/api/store/load-more-products", {data})
-    console.log(p.data.products);
-
-    setLoadedProducts((prevLoadedProducts) => [
-      ...prevLoadedProducts,
-      ...p.data.products.data.products.edges.map((pro) => pro)
-    ]);
-    return
-
-  }
 
 
   const productsRef = useRef<HTMLDivElement>(null);
@@ -150,10 +29,10 @@ useEffect(()=> {
     
     if (productsRef.current) {
       if (direction === 'left') {
-        productsRef.current.scrollLeft -= 400;
+        productsRef.current.scrollLeft -= 800;
       }
       if (direction === 'right') {
-        productsRef.current.scrollLeft += 400;
+        productsRef.current.scrollLeft += 800;
       }
     }
   };
@@ -170,7 +49,7 @@ useEffect(()=> {
                   </Typography>
                 </div>
     
-                <div className='hidden sm:flex  p-3 w-1/5'>
+                <div className='hidden sm:flex  p-3 sm:w-1/5'>
                   <Box className="rounded h-[40vh] w-[17vw] p-3 md:flex md:justify-center md:items-center" sx={{bgcolor:grey[900]}}>
                       <Typography variant='h4' className=' gradient-text' sx={{}} component="div">
                       Products. <span style={{color: "#EEE"}} className='font-normal' >Meticulously Curated for a Lifestyle Worth Living.</span>
@@ -178,7 +57,7 @@ useEffect(()=> {
                   </Box>
                 </div>
 
-                <div className='flex items-center w-4/5'>
+                <div className='flex items-center sm:w-4/5'>
                   <div className='hidden lg:flex absolute '>
                       <Button className='' sx={{color: teal[200],}}   onClick={() => handleProductsNav('left')}>
                         <ArrowCircleLeftRoundedIcon sx={{fontSize: "3rem"}} />
@@ -187,57 +66,7 @@ useEffect(()=> {
                   <div className='flex overflow-x-auto md:overflow-x-hidden w-full gap-12 sm:gap-32 p-3 pb-6' ref={productsRef}>
 
                     
-                    {
-                      products.map((product, i) => {
-                        if(loadedProducts.length === 0 && i === products.length - 1) {
-                          return (
-                            <div className='flex' key={` ${product.node.id} productsPage`}>
-                              <ProductCard goToProductPage={goToProductPage} product={product} />
-                             
-                                <div ref={loadMoreRef} className='h-[20vh] w-[20vw]'>
-                                  <Typography variant='caption' className='gradient-text-home' sx={{}} component="div">
-                                    Loading more products...
-                                  </Typography>
-                                </div>                                
-                        
-                            </div>
-                          )
-                        }
-                        
-                        return (
-                          <div key={` ${product.node.id} productsPage`}>
-                            <ProductCard goToProductPage={goToProductPage} product={product} />
-                          </div>
-                        )
-                      })
-                    }
-                    {
-                      loadedProducts.length > 0 &&
-                      <>
-                        {
-                          loadedProducts.map((product, i) => {
-                            if(loadedProducts.length > 0 && i === loadedProducts.length - 1) {
-                            return (
-                              <div className='flex' key={` ${product.node.id} productsPage`}>
-                                <ProductCard goToProductPage={goToProductPage} product={product} />
-  
-                                <div ref={loadMoreTwoRef} className='h-full flex justify-center items-center'>
-                                  <Typography variant='caption' className='gradient-text-home' sx={{}} component="div">
-                                    Loading more products...
-                                  </Typography>
-                                </div>
-                              </div>
-                            )
-                          }
-                            return (
-                              <div key={` ${product.node.id} productsPage`}>
-                                <ProductCard goToProductPage={goToProductPage} product={product} />
-                              </div>
-                            )
-                          })
-                        }                      
-                      </>
-                    }
+                    <ProductContainer products={products} />
                   </div>
                   <div className='hidden lg:flex absolute right-0'>
                       <Button sx={{color: teal[200], }}  onClick={() => {handleProductsNav('right')}}>
